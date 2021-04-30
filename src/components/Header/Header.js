04b9dd428cb0b62b4306998733f64bd3f33310bb
robertr710 <rrodriguez45@csustan.edu>
2021-04-30 15:34:03 -0700

    Updated title on index.html file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,2 +1,19 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/styles";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+
+}));
+
+const Header = (props) => {
+    return;
+}
+export default Header;
 
